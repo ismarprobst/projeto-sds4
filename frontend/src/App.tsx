694e1dcd7f2db75ec2a46e9886d54680a8c38565
ismarps <ismarprobst@gmail.com>
2021-09-06 20:04:40 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <h1 className="text-primary">Ola mundo!</h1>
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Ola mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
