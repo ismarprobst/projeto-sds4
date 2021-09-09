f75abe3ce4e240fcc32600d55c4a6411f83f1f9f
ismarps <ismarprobst@gmail.com>
2021-09-09 16:06:43 -0300

    Group BY search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsvendas.controllers;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,4 +26,18 @@ public class SaleController {
         Page<SaleDTO> list =service.findAll(pageable);
         return ResponseEntity.ok(list);
     }
+
+    @GetMapping(value = "/sum-by-seller")
+    public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+        List<SaleSumDTO> list =service.amountGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/success-by-seller")
+    public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+        List<SaleSuccessDTO> list =service.successGroupedBySeller();
+        return ResponseEntity.ok(list);
+    }
+
+
 }
