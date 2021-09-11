3d044d9dbc2b0f85e5a9214ff1f76d9605d6e316
ismarps <ismarprobst@gmail.com>
2021-09-11 20:33:24 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,9 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/footer";
-import NavBar from "components/navbar";
+
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Sales Dashboard</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5>Success Rate(%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">All sales</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary"> All sales</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+     <Routes />
   );
 }
 
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div className="div">
+          <h1>Página Home</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
