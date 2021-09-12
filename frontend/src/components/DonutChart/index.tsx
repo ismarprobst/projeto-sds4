2c1cb53225282238a14a9647c606af426ccaf788
ismarps <ismarprobst@gmail.com>
2021-09-12 17:43:56 -0300

    BarChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -10,7 +10,7 @@ type ChartData = {
 };
 
 function DonutChart() {
-    
+
   const [chartData, setChartData] = useState<ChartData>({
     labels: [],
     series: [],
@@ -25,11 +25,6 @@ function DonutChart() {
       setChartData({ labels: myLabels, series: mySeries });
     });
   }, []);
-
-  /* const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }*/
 
   const options = {
     legend: {
